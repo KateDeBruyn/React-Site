1fcf9d7372a2e545f189e6818979e192410f9536
KateDeBruyn <1444800@students.wits.ac.za>
2023-05-15 14:28:22 +0200

    Proper project set up

---
+++ notes.js
@@ -0,0 +1,16 @@
+// Define the Component
+// class Tweet{
+
+//     Define what you want the component to have.
+//     The state is the data that this component will have when it is first rendered/loaded. State can change. 
+//     state = {};
+   
+//     Define how you want it to look and what you want it to do.
+//     Renders the state you can to see
+//     render(){
+
+//     }
+// }
+
+// React users a virtual DOM. Like when you send out a request, the browser gives back a response (Java, HTML, CSS // JSX), browser builds DOM and then renders it. 
+// React keeps a copy of the browser DOM in memory so that it's much quicker. Recognises a state change, computes the difference, and re-renders. 
