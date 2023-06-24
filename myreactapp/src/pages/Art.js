effd53285da92ac93895fe92bb4d0b0944059c0a
KateDeBruyn <1444800@students.wits.ac.za>
2023-06-24 20:12:11 +0200

    Embedded Artwork

--- myreactapp/src/pages/Art.js
+++ myreactapp/src/pages/Art.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import '../styles/Theory.css'
+import '../styles/Theory.css';
+import '../styles/Art.css';
 
 import ModernGreen from '../assets/pics/modern_floral_green.png';
 
@@ -11,6 +12,7 @@ export default class Art extends Component {
         <article className='foreground'>
             <section>
             <h1 className='main-heading'>Artwork Placeholder</h1>
+            <iframe src="https://i.simmer.io/@Keightyrose/fio-interface" className='internet-art'></iframe>
             </section>
         </article>    
       </div>
